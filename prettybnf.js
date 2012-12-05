92d53b54157691d56bd5a93b61914fd56af60cf9
Daniel Connelly <dhconnelly@gmail.com>
2012-12-05 18:14:31 -0500

    parse ws

--- prettybnf.js
+++ prettybnf.js
@@ -4,5 +4,39 @@
 exports.version = '0.0.0';
 exports.parse = function (g) {};
 exports.stringify = function (ast) {};
+exports.Parser = Parser;
+
+// ---------------------------------------------------------------------------
+
+function Parser(input) {
+    var o = (this !== exports) ? this : {};
+    o.input = input;
+    o.pos = 0;
+    o.line = 1;
+}
+
+Parser.EOF = -1;
+
+Parser.prototype.peek = function () {
+    if (this.pos >= this.input.length) return Parser.EOF;
+    return this.input[this.pos];
+};
+
+Parser.prototype.eat = function () {
+    if (this.pos >= this.input.length) return Parser.EOF;
+    return this.input[this.pos++];
+};
+
+// <ws> ::= <space> <ws> | <empty>;
+// <space> ::= " " | "\n" | "\t";
+// <empty> ::= "";
+Parser.prototype.ws = function () {
+    var ret = '', ch;
+    while (' \n\t'.indexOf(ch = this.peek()) >= 0) {
+        if (ch === '\n') this.line++;
+        ret += this.eat();
+    }
+    return ret;
+};
 
 }(typeof exports === 'undefined' ? this.prettybnf = {} : exports));
--- test_prettybnf.js
+++ test_prettybnf.js
@@ -1,6 +1,41 @@
+/*jshint node: true */
+'use strict';
+
 var prettybnf = require('./prettybnf');
 
 exports.testVersion = function (t) {
     t.equal(prettybnf.version, '0.0.0');
+    t.done();
+};
+
+// ---------------------------------------------------------------------------
+
+var Parser = prettybnf.Parser;
+
+exports.testParser_peek = function (t) {
+    var p = new Parser('abc');
+    t.equal(p.peek(), 'a');
+    t.equal(p.peek(), 'a');
+    p.pos = 3;
+    t.equal(p.peek(), Parser.EOF);
+    t.equal(p.peek(), Parser.EOF);
+    t.done();
+};
+
+exports.testParser_eat = function (t) {
+    var p = new Parser('abc');
+    t.equal(p.eat(), 'a');
+    t.equal(p.eat(), 'b');
+    t.equal(p.eat(), 'c');
+    t.equal(p.eat(), Parser.EOF);
+    t.equal(p.eat(), Parser.EOF);
+    t.done();
+};
+
+exports.testParser_ws = function (t) {
+    var p = new Parser('  \n\n   \t\n    foo');
+    t.equal(p.ws(), '  \n\n   \t\n    ');
+    t.equal(p.peek(), 'f');
+    t.equal(p.ws(), '');
     t.done();
 };
